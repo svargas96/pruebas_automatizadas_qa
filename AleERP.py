3c657053aba9da021b9c330d5ce994dee4535d6c
LAPTOP-TGG5BNNN\Sergio Vargas <sergioandres-196@hotmail.com>
2021-12-26 23:00:33 -0500

    se agrega el proyecto

---
+++ AleERP.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+import allure
+import time
+
+class TestInicio(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome('chromedriver.exe')
+
+    def test_login(self):
+       driver = self.driver
+       driver.get('https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+       driver.find_element_by_id('username').clear()
+       driver.find_element_by_id('username').send_keys('sergioandres-196@hotmail.com')
+       driver.find_element_by_id('password').clear()
+       driver.find_element_by_id('password').send_keys('1075297647Sergio')
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
