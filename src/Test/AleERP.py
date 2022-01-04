fdf4c1eb62b9097752021c616c50fe68e5f7b940
svargas96 <sergioandres-196@hotmail.com>
2022-01-04 11:35:36 -0500

    Se cambian localizadores para User y Password y en login AleERP

--- src/Test/AleERP.py
+++ src/Test/AleERP.py
@@ -1,9 +1,7 @@
 # important libraries
 from selenium import webdriver
 import unittest
-import allure
 import time
-import pytest
 
 
 class TestInicio(unittest.TestCase):
@@ -13,10 +11,12 @@ class TestInicio(unittest.TestCase):
     def test_login(self):
         driver = self.driver
         driver.get('https://qa.nuevoerp.co/login')
-        driver.find_element_by_xpath("//*[@id='frmInicioSesion']/div[2]/div/input").clear()
-        driver.find_element_by_xpath("//*[@id='frmInicioSesion']/div[2]/div/input").send_keys('crisfalt@gmail.com')
-        driver.find_element_by_xpath("//*[@id='frmInicioSesion']/div[3]/div/input").clear()
-        driver.find_element_by_xpath("//*[@id='frmInicioSesion']/div[3]/div/input").send_keys('crisfalt')
+        driver.find_element_by_xpath('/html/body/app-root/app-login/div/div[4]/div/div/form/div[2]/div/input').clear()
+        driver.find_element_by_xpath(
+            '/html/body/app-root/app-login/div/div[4]/div/div/form/div[2]/div/input').send_keys('crisfalt@gmail.com')
+        driver.find_element_by_name('password').clear()
+        driver.find_element_by_name('password').send_keys('crisfalt')
+
         time.sleep(10)
 
     def tearDown(self):
