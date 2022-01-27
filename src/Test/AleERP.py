e4310472e30337818afba291d9865f4663de724c
svargas96 <sergioandres-196@hotmail.com>
2022-01-26 23:01:36 -0500

    actualización de codigo

--- src/Test/AleERP.py
+++ src/Test/AleERP.py
@@ -11,11 +11,11 @@ class TestInicio(unittest.TestCase):
     def test_login(self):
         driver = self.driver
         driver.get('https://qa.nuevoerp.co/login')
-        driver.find_element_by_xpath("//*[@id='frmInicioSesion']/div[2]/div/input").clear()
-        driver.find_element_by_xpath(
-            "//*[@id='frmInicioSesion']/div[2]/div/input").send_keys('svargasmurcia.sinergia@gmail.com')
-        driver.find_element_by_name('password').clear()
-        driver.find_element_by_name('password').send_keys('svargas')
+        driver.find_element_by_xpath("//body/app-root[1]/app-login[1]/div[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]").clear()
+        driver.find_element_by_xpath("//body/app-root[1]/app-login[1]/div[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]").send_keys('prueba@gmail.com')
+        driver.find_element_by_xpath("//body/app-root[1]/app-login[1]/div[1]/div[4]/div[1]/div[1]/form[1]/div[3]/div[1]/input[1]").clear()
+        driver.find_element_by_xpath("//body/app-root[1]/app-login[1]/div[1]/div[4]/div[1]/div[1]/form[1]/div[3]/div[1]/input[1]").send_keys('prueba12345')
+        driver.find_element_by_xpath("//button[contains(text(),'Ingresar')]").click()
         time.sleep(10)
 
     def tearDown(self):
